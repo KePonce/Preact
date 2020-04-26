b0481bf038a04de845c070abe30fe1107cfbf48e
kepch77 <kep189pon@gmail.com>
2020-04-26 14:51:52 -0600

    Pagina Ingreso de Datos

--- Login/src/index.js
+++ Login/src/index.js
@@ -1,4 +1,8 @@
 import './style';
+import'bootstrap/dist/css/bootstrap.min.css';
+import $ from'jquery';
+import Popper from'popper.js';
+import'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './components/app';
 
 export default App;
