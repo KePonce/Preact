f09da6cb89434c9ec46ded880b578aa6fc6964eb
kepch77 <kep189pon@gmail.com>
2020-04-26 01:41:13 -0600

    Base Preact
    Instalación herramienta y página base para desarrollo

---
+++ Login/src/components/header/index.js
@@ -0,0 +1,15 @@
+import { h } from 'preact';
+import { Link } from 'preact-router/match';
+import style from './style.css';
+
+const Header = () => (
+	<header class={style.header}>
+		<h1>Bono App</h1>
+		<nav>
+			<Link activeClassName={style.active} href="/">Bienvenidos</Link>
+			<Link activeClassName={style.active} href="/profile">Ingreso</Link>
+		</nav>
+	</header>
+);
+
+export default Header;
