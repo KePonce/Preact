6f2791d284bfc562284e7ab66ddade4d8af0c296
kepch77 <kep189pon@gmail.com>
2020-04-27 23:42:54 -0600

    Refrescar
    Direccionar ingreso en backend

--- Recaptcha_BackEnd/Main.js
+++ Recaptcha_BackEnd/Main.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'build')))
 app.use(express.json()  );
 
 
-app.get('/',function(req,res){
+app.get('/ingreso',function(req,res){
     res.sendFile(path.join(__dirname, 'build','index.html'));
 
 });
